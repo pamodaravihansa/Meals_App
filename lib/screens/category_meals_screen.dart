6653229b3a604cbda911c343e9cfa648c994b5a5
pamodaravihansa <pamoda.ravihansa@gmail.com>
2022-09-01 07:05:52 +0530

    organized file structure

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
-import './dummy_data.dart';
+import '../dummy_data.dart';
 
 class CategoryMeals extends StatelessWidget {
   // final String categoryId;
