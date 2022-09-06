59156cd49a967c1a86ec1b22568415bf93f136ef
pamodaravihansa <pamoda.ravihansa@gmail.com>
2022-09-06 12:55:23 +0530

    added image in meal detail screen

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../dummy_data.dart';
 
 class MealDetail extends StatelessWidget {
   const MealDetail({Key key}) : super(key: key);
@@ -7,12 +8,23 @@ class MealDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final mealId = ModalRoute.of(context).settings.arguments as String;
+    final selectedMeal = DUMMY_MEALS.firstWhere((meal) => meal.id == mealId);
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(mealId),
+        title: Text('${selectedMeal.title}'),
       ),
-      body: Center(
-        child: Text('This is meal! - $mealId'),
+      body: Column(
+        children: [
+          Container(
+            height: 300,
+            width: double.infinity,
+            child: Image.network(
+              selectedMeal.imageUrl,
+              fit: BoxFit.cover,
+            ),
+          ),
+        ],
       ),
     );
   }
